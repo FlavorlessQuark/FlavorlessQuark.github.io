6ad8684b144a419a36fdb76ba612157c91804a71
flavorlessquark <flavorlessquark@gmail.com>
2022-08-16 10:42:38 -0700

    Home Page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -14,9 +14,18 @@ export const Home = ({ children }) =>
 					World!
 				</Hello>
 			</div>
-			<Text>
-				Text here
-			</Text>
+			<FlexText style={{textAlign: "start"}}>
+				<Text>
+					I'm a programmer and C wizard!
+				</Text>
+				<br/>
+				<Text>
+					I enjoy creating programming tools and have been working as FullStack / Web3/ Smart Contract developer
+				</Text>
+			</FlexText>
+				<Text style={{textAlign: "center", alignSelf: "center"}}>
+					This website is a showcase of the code I have crafted
+				</Text>
 		</Container>
     )
 }
@@ -24,11 +33,11 @@ const Container = styled.div`
 	display:flex;
 	flex-direction: column;
 	font-family:"Iceland";
-	justify-content: space-around;
 	width: 100%;
 	padding: 80px;
 	align-items: flex-start;
-	justify-content: flex-start;
+	justify-content: space-between;
+	gap: 20px;
 `
 
 const Hello = styled.div`
@@ -37,9 +46,17 @@ const Hello = styled.div`
 	color: ${props =>props.theme.colors.secondary};
 `
 
+const FlexText = styled.div`
+	display:flex;
+	flex-direction: column;
+	text-align: start;
+	// align-self: end;
+	gap: 20px;
+	max-width: 940px;
+`
+
 const Text = styled.div`
 	display:flex;
-	font-size:114px;
+	font-size:48px;
 	color: ${props =>props.theme.colors.shadow};
-	align-self: end;
 `
